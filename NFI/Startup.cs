ee34bb6f367240bb998f388912feac973492d76a
monjurul habib <monjurul_habib@hotmail.com>
2015-11-13 11:11:29 +0600

    initial setup

---
+++ NFI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NFI.Startup))]
+namespace NFI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
