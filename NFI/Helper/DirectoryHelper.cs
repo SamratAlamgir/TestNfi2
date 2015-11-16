b4e1454d588b4929071d7180dc54e1e018b144da
Samrat Alamgir <alamgir.samrat@outlook.com>
2015-11-16 16:56:26 +0600

    #69: JsonHelper and DirectoryHelper class added.  - Few settings added

---
+++ NFI/Helper/DirectoryHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using NFI.Enums;
+using NFI.Properties;
+
+namespace NFI.Helper
+{
+    public static class DirectoryHelper
+    {
+        public static string GetRootDirectory()
+        {
+            return Settings.Default.RootDir;
+        }
+
+        public static string GetApplicationDirPath(ApplicationType appType)
+        {
+            var dirPath = "";
+
+            if (appType == ApplicationType.Application1)
+            {
+                dirPath = Settings.Default.ApplicationDir1;
+            }
+
+            return dirPath;
+        }
+
+        public static string GetApplicationDataFilePath(ApplicationType appType)
+        {
+            var fileName = "";
+
+            if (appType == ApplicationType.Application1)
+            {
+                fileName = Settings.Default.ApplicationDataFile;
+            }
+
+            var filePath = Path.Combine(GetApplicationDirPath(appType), fileName);
+
+            return filePath;
+        }
+    }
+}
