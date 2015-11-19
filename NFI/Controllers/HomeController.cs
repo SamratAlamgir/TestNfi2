62f1210d05d56f41e9f22794f94835f8b8b48a9a
monjurul habib <monjurul_habib@hotmail.com>
2015-11-19 13:38:49 +0600

    cleanup routing

--- NFI/Controllers/HomeController.cs
+++ NFI/Controllers/HomeController.cs
@@ -10,25 +10,7 @@ namespace NFI.Controllers
     public class HomeController : Controller
     {
         private const string TimestampPattern = "yyyyMMddHHmmssfff";
-        public ActionResult Index()
-        {
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+      
 
         public ActionResult InputWizard()
         {
