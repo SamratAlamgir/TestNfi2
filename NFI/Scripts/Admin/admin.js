e408d1d0e4f3d8ecedb8946d44f62182f11c189c
Samrat Alamgir <alamgir.samrat@outlook.com>
2015-11-19 12:58:18 +0600

    #77: Attachment download option added

--- NFI/Scripts/Admin/admin.js
+++ NFI/Scripts/Admin/admin.js
@@ -3,17 +3,23 @@
     $.getJSON("/admin/GetApplications")
         .done(function (data) {
 
-            $('#example').DataTable({
+            var table = $('#applicationListTable').DataTable({
+                "iDisplayLength": 25,
                 "aaData": data,
                 "aoColumns": [
                     { "mDataProp": "Name" },
                     { "mDataProp": "Email" },
                     { "mDataProp": "Sex" },
                     { "mDataProp": "Company" },
-                    { "mDataProp": "FilePath" }
-                ]
+                    {
+                        "mDataProp": "FilePath",
+                        "fnCreatedCell": function (nTd, sData, oData) {
+                            $(nTd).html("<a href='/" + oData.FilePath + "'>Download</a>");
+                        }
+                    }
+                ],
+                "columnDefs": [{ "targets": 4, "orderable": false, "searchable": false }]
             });
-
         })
     .fail(function () {
         alert("Fail");
