974be42d36f9015eb660978ce10ae1a5a17a6fbe
rksajib <sajib.cefalo@hotmail.com>
2015-11-23 09:53:11 +0600

    #forms authentication added

--- NFI/App_Start/CaptchaAuthorizeAttribute.cs
+++ NFI/App_Start/CaptchaAuthorizeAttribute.cs
@@ -1,34 +1,18 @@
 ﻿using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 
 namespace NFI.App_Start
 {
     public class CaptchaAuthorizeAttribute : AuthorizeAttribute
     {
-        public override void OnAuthorization(AuthorizationContext filterContext)
+       protected override bool AuthorizeCore(HttpContextBase httpContext)
         {
-            base.OnAuthorization(filterContext);
             var captchaVerified = false;
-            if (filterContext.HttpContext.Session?["IsCaptchaVerfied"] != null)
+            if (httpContext.Session?["IsCaptchaVerfied"] != null)
             {
-                captchaVerified = (bool)filterContext.HttpContext.Session["IsCaptchaVerfied"];
+                captchaVerified = (bool)httpContext.Session["IsCaptchaVerfied"];
             }
-            if (!captchaVerified)
-            {
-                filterContext.Result = new RedirectToRouteResult(
-                    new RouteValueDictionary {
-                            { "controller", "Captcha" },
-                            { "action", "Index" },
-                            { "ReturnUrl", filterContext.HttpContext.Request.RawUrl }
-                    });
-            }
-
-        }
-
-        protected override bool AuthorizeCore(HttpContextBase httpContext)
-        {
-            return true;
+            return captchaVerified;
         }
     }
 }
