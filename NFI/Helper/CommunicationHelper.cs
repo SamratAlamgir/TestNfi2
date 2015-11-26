3c07339b7d3607a3137d52142c2c317754e6272a
Samrat Alamgir <alamgir.samrat@outlook.com>
2015-11-26 12:49:17 +0600

    #148: Mail for applicant added

--- NFI/Helper/CommunicationHelper.cs
+++ NFI/Helper/CommunicationHelper.cs
@@ -10,10 +10,25 @@ namespace NFI.Helper
     {
         public static bool SendMailToExecutive(Dictionary<string, string> values, string mailTo)
         {
-            var subject = "Hello <UserName>";
+            var subject = "Application submitted by <UserName>";
             var body = "A new application of type <ApplicationType> has been submitted." + Environment.NewLine +
                        "Download zip file: <ZipFileLink>" + Environment.NewLine +
                        "View detail: <DetailViewLink>";
+
+            foreach (var key in values.Keys)
+            {
+                subject = subject.Replace(key, values[key]);
+                body = body.Replace(key, values[key]);
+            }
+
+            return Emailer.SendMail(mailTo, subject, body);
+        }
+
+        public static bool SendMailToApplicant(Dictionary<string, string> values, string mailTo)
+        {
+            var subject = "Application type <ApplicationType> submitted";
+            var body = "Hi <UserName>," + Environment.NewLine +
+                       "Thanks for submitting the application";
 
             foreach (var key in values.Keys)
             {
