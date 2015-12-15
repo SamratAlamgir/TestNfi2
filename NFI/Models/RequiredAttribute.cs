2d98c1aa82a0dc7ac0cbbaa2369f8dc3e28882ea
rksajib <sajib.cefalo@hotmail.com>
2015-12-15 09:55:49 +0600

    #new attribute added

---
+++ NFI/Models/RequiredAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NFI.Models
+{
+    public class RequiredAttribute : System.ComponentModel.DataAnnotations.RequiredAttribute
+    {
+        private string _displayName;
+
+        public RequiredAttribute()
+        {
+            ErrorMessage = "{0} er obligatorisk";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var attributes =
+                validationContext.ObjectType.GetProperty(validationContext.MemberName)
+                    .GetCustomAttributes(typeof (DisplayNameAttribute), true);
+            var displayNameAttribute = attributes[0] as DisplayNameAttribute;
+            if (displayNameAttribute != null)
+                _displayName = displayNameAttribute.DisplayName;
+
+            return base.IsValid(value, validationContext);
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(ErrorMessageString, _displayName);
+        }
+       
+    }
+}
