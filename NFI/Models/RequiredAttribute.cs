94692531762eafad3d1a5c7ae166a935c11d3936
rksajib <sajib.cefalo@hotmail.com>
2015-12-21 09:58:49 +0600

    validation message changed

--- NFI/Models/RequiredAttribute.cs
+++ NFI/Models/RequiredAttribute.cs
@@ -1,26 +1,35 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace NFI.Models
 {
-    public class RequiredAttribute : System.ComponentModel.DataAnnotations.RequiredAttribute
+    public class RequiredAttribute : System.ComponentModel.DataAnnotations.RequiredAttribute, IClientValidatable
     {
         private string _displayName;
 
-        public RequiredAttribute()
+        public RequiredAttribute(string language = "nok")
         {
-            ErrorMessage = "{0} er obligatorisk";
+            ErrorMessage = language == "nok" ? "Dette feltet er påkrevd." : "This field is required.";
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var attributes =
                 validationContext.ObjectType.GetProperty(validationContext.MemberName)
-                    .GetCustomAttributes(typeof (DisplayNameAttribute), true);
-            var displayNameAttribute = attributes[0] as DisplayNameAttribute;
-            if (displayNameAttribute != null)
-                _displayName = displayNameAttribute.DisplayName;
-
+                    .GetCustomAttributes(typeof(DisplayNameAttribute), true).ToList();
+            if (attributes.Count > 0)
+            {
+                var displayNameAttribute = attributes[0] as DisplayNameAttribute;
+                if (displayNameAttribute != null)
+                {
+                    _displayName = displayNameAttribute.DisplayName;
+                    return base.IsValid(value, validationContext);
+                }
+            }
+            _displayName = validationContext.MemberName;
             return base.IsValid(value, validationContext);
         }
 
@@ -28,6 +37,15 @@ namespace NFI.Models
         {
             return string.Format(ErrorMessageString, _displayName);
         }
-       
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metaData, ControllerContext context)
+        {
+            var rule = new ModelClientValidationRule
+            {
+                ValidationType = "required",
+                ErrorMessage = FormatErrorMessage(metaData.DisplayName)
+            };
+            yield return rule;
+        }
+
     }
 }
